6baf10e88e9ff0cc5d49322ec6a7e2568d7ac24d
毯 <166950178+tan0724@users.noreply.github.com>
2024-05-07 02:01:59 +0800

    新增測試地毯機器人

---
+++ bot.py
@@ -0,0 +1,61 @@
+import discord
+import asyncio
+from dotenv import load_dotenv
+import os
+from discord.ext import commands
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.messages = True
+bot = commands.Bot(command_prefix='%', intents=intents)
+
+TOKEN = os.getenv("TOKEN")
+if TOKEN is None:
+    print("錯誤：找不到 Discord 令牌。請設置 TOKEN 環境變數。")
+    exit()
+
+@bot.event
+async def on_ready():
+    slash = await bot.tree.sync()
+    print(f"目前登入身份 --> {bot.user}")
+    print(f"載入 {len(slash)} 個斜線指令")
+
+# 載入指令程式檔案
+@bot.command()
+async def load(ctx, extension):
+    await bot.load_extension(f"cogs.{extension}")
+    await ctx.send(f"Loaded {extension} done.")
+    slash = await bot.tree.sync()
+    print(f"載入 {len(slash)} 個斜線指令")
+
+# 卸載指令檔案
+@bot.command()
+async def unload(ctx, extension):
+    await bot.unload_extension(f"cogs.{extension}")
+    await ctx.send(f"UnLoaded {extension} done.")
+    slash = await bot.tree.sync()
+    print(f"載入 {len(slash)} 個斜線指令")
+
+# 重新載入程式檔案
+@bot.command()
+async def reload(ctx, extension):
+    await bot.reload_extension(f"cogs.{extension}")
+    await ctx.send(f"ReLoaded {extension} done.")
+    slash = await bot.tree.sync()
+    print(f"載入 {len(slash)} 個斜線指令")
+    
+# 一開始bot開機需載入全部程式檔案
+async def load_extensions():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(TOKEN)
+
+# 確定執行此py檔才會執行
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ cogs/TaskBase.py
@@ -0,0 +1,40 @@
+import time
+import discord
+from discord.ext import commands
+from discord.ext import tasks
+
+
+class TaskBase(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        # 開始執行函式
+        self.count.start()
+        self.start_time = time.time()
+
+    def cog_unload(self):
+        # 取消執行函式
+        self.count.cancel()
+
+    # 定義要執行的循環函式
+    @tasks.loop(seconds = 6)
+    async def count(self):
+        execution_time = int(time.time() - self.start_time)
+        # 偵測所在伺服器數
+        guilds = len(self.bot.guilds)
+        # 偵測使用者人數
+        count = sum(len(guild.members) for guild in self.bot.guilds)
+        try:
+            # 等待bot ready
+            await self.bot.wait_until_ready()
+            # 變更bot狀態
+            await self.bot.change_presence(activity=discord.Game(name=f"{guilds}個伺服器 | {count}個人"))
+            
+        except Exception as erro:
+            # 錯誤輸出
+            print(erro)
+
+
+
+# Cog 載入 Bot 中
+async def setup(bot: commands.Bot):
+    await bot.add_cog(TaskBase(bot))
